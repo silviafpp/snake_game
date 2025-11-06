211ef8b655dc065752ae328e5ca077d584d421d8
Silvia Pacheco <silviapacheco724@gmail.com>
2025-11-06 11:35:50 -0100

    config

---
+++ game.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+from snake import Snake
+from apple import Apple
+
+WIDTH, HEIGHT = 1920, 1080
+TILE_SIZE = 20
+GREEN = (140, 215, 120)
+DARK_GREEN = (130, 205, 110)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
